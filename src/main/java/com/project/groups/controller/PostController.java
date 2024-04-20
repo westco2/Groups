158b551583de928a2124d56e704338a518581870
YeonHaw <kyhhh2682@gmail.com>
2024-04-20 18:39:11 +0900

    연화 컨플릭 수정

--- src/main/java/com/project/groups/controller/PostController.java
+++ src/main/java/com/project/groups/controller/PostController.java
@@ -1,3 +1,28 @@
 package com.project.groups.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("/posts")
+@Controller
+public class PostController {
+    @GetMapping("/register")
+    public void register() {
+    }
+
+    @GetMapping("/detail")
+    public void showDetail() {
+    }
+
+    @GetMapping("/update")
+    public void home() {
+
+    }
+
+    @GetMapping("/notilist")
+    public void notilist() { 
+        
+    }
+}
--- src/main/java/com/project/groups/postY.service/PostService.java
+++
@@ -1,6 +0,0 @@
-package com.project.groups.postY.service;
-
-
-public class PostService {
-
-}
